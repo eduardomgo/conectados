bc0f8e32d3849b034820c07029b54a99ddece43a
Eduardo <eduardomg@id.uff.br>
2019-09-15 17:24:05 -0300

    daaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaale

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and 
+  has_one_attached :avatar
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
