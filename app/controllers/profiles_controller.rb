12d3f678d9acd0b4ea14c94d8fc71999be517a5b
Eduardo <eduardomg@id.uff.br>
2019-09-16 19:34:41 -0300

    amizade

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-    before_action :set_user, only: [:show, :destroy, :add_friend]
+    before_action :set_user, only: [:destroy_friendship, :accept_friendship, :friends, :show, :destroy, :add_friend]
     before_action :block_not_adm, only: :destroy
 
     def block_not_adm
@@ -9,13 +9,58 @@ class ProfilesController < ApplicationController
             redirect_to new_user_session_path
         end
     end
+    
+    def accept_friendship
+        @friend = Friend.find_by(replyer_id: @user.id, asker_id: current_user.id)
+        puts @user.id, current_user.id
+        @friend.accepted = true
+        @friend.save
+        redirect_to profile_path(@user.id)
+    end
+
+    def destroy_friendship
+        @friend = Friend.find_by(asker_id: @user.id, replyer_id: current_user.id)
+        @friend2 = Friend.find_by(asker_id: current_user.id, replyer_id: @user.id)
+        @friend.destroy
+        @friend2.destroy
+        redirect_to profiles_path(current_user.id)
+    end
+
+    def friends
+        @friend_asks = Friend.where(asker_id: @user.id, accepted: false)
+        @f1 = Friend.where(asker_id: @user.id, accepted: true)
+        @friends1 = []
+        @f1.each do |f|
+            @friends1.push([f.replyer.name, f.replyer_id])
+        end
+
+        @f2 = Friend.where(replyer: @user.id, accepted: true)
+        @friends2 = []
+        @f2.each do |f|
+            @friends2.push(f.asker.name)
+        end
+
+        @friends = []
+        @friends1.each do |f|
+            if f[0].in?(@friends2)
+                @friends.push(f)
+            end
+        end
+    end
 
     def add_friend
         if Friend.where(asker_id: current_user.id, replyer_id: @user.id).count() == 0
-            Friend.create(asker_id: current_user.id, replyer_id: @user.id)
+            Friend.create(asker_id: current_user.id, replyer_id: @user.id, accepted: true)
             Friend.create(asker_id: @user.id, replyer_id: current_user.id)
         end
+        redirect_to profile_path(@user.id)
     end
+
+    def show
+        @friends = Friend.where(asker_id: @user.id)
+        @is_friend = Friend.where(asker_id: current_user.id, replyer_id: @user.id).count()
+    end
+    
 
     def destroy
         @user.destroy
@@ -24,15 +69,13 @@ class ProfilesController < ApplicationController
 
     def index
         @users = if params[:term]
-            User.where("name LIKE (?)",
-            "%#{params[:term]}%").paginate(page: params[:page], per_page: 10)
+            User.where("name LIKE (?)", "%#{params[:term]}%").paginate(page: params[:page], per_page: 10)
         else
           User.paginate(page: params[:page], per_page: 6)
         end
     end
 
     def show
-        @friends = Friend.where(asker_id: @user.id)
         @is_friend = Friend.where(asker_id: current_user.id, replyer_id: @user.id).count()
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get '/profiles/:id', to: 'profiles#show', as: :profile
   get '/profiles', to: 'profiles#index', as: :profiles
   post '/friendship/:id', to: 'profiles#add_friend', as: :add
+  get '/profiles/:id/friends', to: 'profiles#friends', as: :friends
+  post '/profiles/:id/friends/accept', to: 'profiles#accept_friendship', as: :accept_friendship
+  delete '/remove_fs/:id', to: 'profiles#destroy_friendship', as: :destroy_friendship
 
   root to: 'static_pages#root'
 end
